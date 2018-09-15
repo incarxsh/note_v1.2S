8a25c77323ea99280717525f904783d475d012b8
谢少华 <775473732@qq.com>
2018-09-15 11:54:54 +0800

    框架搭建
    vue-cli + vue-router + stage-2 + vuex + less

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <p class="p">我是颜色</p>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scope lang="less">
+  #app{
+    .p{
+      color: orange;
+    }
+  }
+</style>
